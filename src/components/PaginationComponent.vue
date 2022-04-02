9b548fe963ce2b1e7153ad433e7c58592e6af7c9
ToshaShigenina <Shigenina@gmail.com>
2022-04-02 15:20:06 +0500

    V-01/F-01

---
+++ src/components/PaginationComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="slider__pagination pagination">
+    <li class="pagination__dot" v-for="i in count" :key="'dot' + i" :class="{ _active: i === active + 1 }">
+      <button type="button" @click="toSlide(i - 1)"></button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "pagination-component",
+  props: {
+    active: {
+      type: Number,
+      require: true,
+    },
+    count: {
+      type: Number,
+      require: true,
+    },
+  },
+  methods: {
+    toSlide(i) {
+      this.$emit("to-slide", i);
+    },
+  },
+};
+</script>
